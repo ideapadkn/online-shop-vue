74f1f18c4dfbb3c71c393ff6ad2316622495f98d
Kamron or Камрон <kamrush6429@gmail.com>
2023-09-16 19:34:25 +0500

    added product details but not working

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,7 +1,10 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, computed } from "vue";
 import { productsStore } from "@/stores/products";
+import { useRouter, useRoute } from "vue-router";
 
+const router = useRouter();
+const route = useRoute();
 const store = productsStore();
 
 onMounted(async () => {
@@ -20,13 +23,19 @@ export default defineComponent({
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
     <div
+      @click="router.push(`/product/${product.id}`)"
       class="product shadow-md border-2 p-2"
       v-for="product in store.products"
       :key="product.id"
     >
       <img class="h-[200px]" :src="product.thumbnail" alt="" />
-      <h2 class="mb-2"><span class="font-semibold">Brand:</span> {{ product.brand }}</h2>
-      <p class="mb-2"><span class="font-semibold">Description:</span> {{ product.description }}</p>
+      <h2 class="mb-2 text-2xl">
+        <span class="font-semibold">Brand:</span> {{ product.brand }}
+      </h2>
+      <p class="mb-2">
+        <span class="font-semibold">Description:</span>
+        {{ product.description }}
+      </p>
       <p><span class="font-semibold">Price:</span> ${{ product.price }}</p>
     </div>
   </div>
---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,42 @@
+<script>
+import { computed } from "vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ProductDetails",
+});
+</script>
+
+<script setup>
+import { computed } from "vue";
+import { productsStore } from "@/stores/products";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const store = productsStore();
+
+const selectedProduct = computed(() => {
+  return store.products.find((item) => {
+    item.id === Number(route.params.id);
+  });
+});
+</script>
+
+<template>
+  <div>
+    <div>
+      <img :src="selectedProduct.thumbnail" alt="" />
+    </div>
+    <div>
+      <h2 class="mb-2 text-2xl">
+        <span class="font-semibold">Brand:</span> {{ selectedProduct.brand }}
+      </h2>
+      <p class="mb-2">
+        <span class="font-semibold">Description:</span>
+        {{ selectedProduct.description }}
+      </p>
+      <p>
+        <span class="font-semibold">Price:</span> ${{ selectedProduct.price }}
+      </p>
+    </div>
+  </div>
+</template>
