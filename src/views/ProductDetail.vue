14439e9930904f1ce3995f1a8461d29322c24c58
Kamron or Камрон <kamrush6429@gmail.com>
2023-09-19 19:47:23 +0500

    bug

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -10,14 +10,13 @@ import { computed } from "vue";
 import { productsStore } from "@/stores/products";
 import { useRoute } from "vue-router";
 
-const route = useRoute();
 const store = productsStore();
+const route = useRoute();
 
 const selectedProduct = computed(() => {
-  return store.products.find((item) => {
-    item.id === Number(route.params.id);
-  });
+  return store.products.find((item) => item.id === Number(route.params.id));
 });
+console.log(selectedProduct.brand);
 </script>
 
 <template>
